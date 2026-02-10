1dcfe57760f5fecd0c7725b5656336603685b5b6
tbakker234567 <tbakker18@st.noorderpoort.nl>
2026-02-10 14:21:14 +0100

    boulder trigger

--- NoorderJam 2026/Assets/Scripts/boulder.cs
+++ NoorderJam 2026/Assets/Scripts/boulder.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class boulder : MonoBehaviour
 {
+
+    Rigidbody2D rb;
     public GameObject player;
     private Vector2 savedPosition;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         savedPosition = player.transform.position;
     }
 
@@ -15,6 +18,14 @@ public class boulder : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
