a6f6bf572e015f71506219046decb10e42f8b456
TristanKemper <tristankemper1000@gmail.com>
2026-02-12 08:48:38 +0100

    Final Time Script update & Time Scene

--- NoorderJam 2026/Assets/Scripts/UI/TimerScript.cs
+++ NoorderJam 2026/Assets/Scripts/UI/TimerScript.cs
@@ -6,22 +6,32 @@ using UnityEngine.SceneManagement;
 public class TimerScript : MonoBehaviour
 {
     TextMeshProUGUI timerText;
-    float TimeTaken;
+    TextMeshProUGUI FinishedTime;
+    public float TimeTaken;
     void Start()
     {
         timerText = GameObject.Find("Timer").GetComponent<TextMeshProUGUI>();
+        FinishedTime = GameObject.Find("FinishTime").GetComponent<TextMeshProUGUI>();
     }
 
     void Update()
     {
-        if (SceneManager.GetActiveScene().name != "Main Menu")
+        if (SceneManager.GetActiveScene().name != "Main Menu" && SceneManager.GetActiveScene().name != "Speedrun Final Time")
         {
             TimeTaken += Time.deltaTime;
             timerText.enabled = true;
+            FinishedTime.enabled = false;
         }
         else if (SceneManager.GetActiveScene().name == "Main Menu")
         {
             timerText.enabled = false;
+            FinishedTime.enabled = false;
+        }
+        else if (SceneManager.GetActiveScene().name == "Speedrun Final Time")
+        {
+            timerText.enabled = false;
+            FinishedTime.enabled = true;
+            FinishedTime.text = "Your Time: " + string.Format("{0:00}:{1:00}", Mathf.FloorToInt(TimeTaken / 60F), Mathf.FloorToInt(TimeTaken - (Mathf.FloorToInt(TimeTaken / 60F) * 60)));
         }
 
         int minutes = Mathf.FloorToInt(TimeTaken / 60F);
